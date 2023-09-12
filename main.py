897185ab945c858fdd8bf01f2e8aebee5f842783
Rohan Dutta <rohanduttaperfect@gmail.com>
2023-09-12 20:43:44 +0530

    📦 NEW: Implemented Scheduler

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import time
 
+from multiprocessing import Queue
+
 from workers.wiki_worker import WikiWorker
-from workers.google_finance_worker import GoogleFinanceWorker
+from workers.google_finance_worker import GoogleFinanceScheduler
 
 
 def main():
+    symbol_queue = Queue()
     scraper_start_time = time.time()
 
-    current_workers = []
-
     wiki_worker = WikiWorker()
-    for symbol in wiki_worker.get_nifty_50_companies():
-        google_finance_worker = GoogleFinanceWorker(symbol=symbol)
-        current_workers.append(google_finance_worker)
+    google_finance_scheduler_threads = []
 
-    for i in range(len(current_workers)):
-        current_workers[i].join()
+    num_google_finance_worker = 10
+    for i in range(num_google_finance_worker):
+        google_finance_scheduler = GoogleFinanceScheduler(input_queue=symbol_queue)
+        google_finance_scheduler_threads.append(google_finance_scheduler)
+
+    for symbol in wiki_worker.get_nifty_50_companies():
+        symbol_queue.put(symbol)
+
+    for i in range(len(google_finance_scheduler_threads)):
+        symbol_queue.put("DONE")
+
+    for i in range(len(google_finance_scheduler_threads)):
+        google_finance_scheduler_threads[i].join()
 
     print("--------------------------------------------------------")
     print("Time Taken : ", round(time.time() - scraper_start_time, 1))
--- workers/google_finance_worker.py
+++ workers/google_finance_worker.py
@@ -3,22 +3,37 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class GoogleFinanceWorker(threading.Thread):
-    def __init__(self, symbol, **kwargs):
-        super(GoogleFinanceWorker, self).__init__(**kwargs)
-        self._symbol = symbol
-        base_url = "https://www.google.com/finance/quote/"
-        self._url = base_url + self._symbol + ":NSE"
+class GoogleFinanceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
+        super(GoogleFinanceScheduler, self).__init__(**kwargs)
+        self._input_queue = input_queue
         self.start()
 
     def run(self):
+        while True:
+            val = self._input_queue.get()
+            if val == "DONE":
+                break
+            google_finance_worker = GoogleFinanceWorker(symbol=val)
+            price = google_finance_worker.get_price()
+            print(price)
+
+
+class GoogleFinanceWorker:
+    def __init__(self, symbol):
+        self._symbol = symbol
+        base_url = "https://www.google.com/finance/quote/"
+        self._url = base_url + self._symbol + ":NSE"
+
+    def get_price(self):
         response = requests.get(self._url)
         if response.status_code != 200:
             return
         soup = BeautifulSoup(response.text, "lxml")
         name = soup.find("div", class_="zzDege").text
         price = soup.find("div", class_="YMlKec fxKbKc").text
-        print(f"({self._symbol}) {name} : {price}")
+
+        return f"({self._symbol}) {name} : {price}"
 
 
 if __name__ == "__main__":
